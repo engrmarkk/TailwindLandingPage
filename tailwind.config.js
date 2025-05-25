cebcc653a314ac4554f3012822d50c9481379ab9
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-25 18:38:12 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html", "./index.html"], // Now checks HTML in /templates
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
